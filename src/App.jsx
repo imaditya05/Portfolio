3179c083785a11b0294a24fd6b0f629ad6a379c7
Aditya <imaditya05@gmail.com>
2023-04-11 22:23:44 +0530

    feat: Modified code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
 const App = () => {
-  return <div className="text-3xl">Hello</div>;
+  return (
+    <div className="w-full px-2 py-6 lg:px-10 lg:w-2/3 mx-auto transition-all duration-150 bg-black flex min-h-screen justify-center items-center ">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
